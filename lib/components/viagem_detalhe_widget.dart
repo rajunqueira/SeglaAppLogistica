f9c61b42d4d26db9ec25abafbf0d2a7ccd712e2c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-19 00:09:13 +0000

    Updating to latest FlutterFlow output.

--- lib/components/viagem_detalhe_widget.dart
+++ lib/components/viagem_detalhe_widget.dart
@@ -265,41 +265,82 @@ class _ViagemDetalheWidgetState extends State<ViagemDetalheWidget> {
                               mainAxisSize: MainAxisSize.max,
                               children: [
                                 Align(
-                                  alignment: AlignmentDirectional(0.0, 0.0),
+                                  alignment: AlignmentDirectional(-1.0, 0.0),
                                   child: Padding(
                                     padding: EdgeInsetsDirectional.fromSTEB(
                                         0.0, 0.0, 0.0, 12.0),
                                     child: Container(
                                       width: MediaQuery.sizeOf(context).width *
                                           0.46,
-                                      height: 130.0,
-                                      child: FlutterFlowPieChart(
-                                        data: FFPieChartData(
-                                          values: _model.dados,
-                                          colors: chartPieChartColorsList,
-                                          radius: [60.0],
-                                        ),
-                                        donutHoleRadius: 0.0,
-                                        donutHoleColor: Colors.transparent,
-                                        sectionLabelType:
-                                            PieChartSectionLabelType.value,
-                                        sectionLabelStyle:
-                                            FlutterFlowTheme.of(context)
-                                                .headlineSmall
-                                                .override(
-                                                  fontFamily: 'Roboto',
-                                                  fontSize: 12.0,
-                                                  letterSpacing: 0.0,
-                                                  fontWeight: FontWeight.normal,
-                                                ),
-                                        labelFormatter: LabelFormatter(
-                                          numberFormat: (val) => formatNumber(
-                                            val,
-                                            formatType: FormatType.custom,
-                                            format: '#,##0.000',
-                                            locale: 'pt-BR',
+                                      height: 120.0,
+                                      child: Stack(
+                                        children: [
+                                          FlutterFlowPieChart(
+                                            data: FFPieChartData(
+                                              values: _model.dados,
+                                              colors: chartPieChartColorsList,
+                                              radius: [30.0],
+                                            ),
+                                            donutHoleRadius: 20.0,
+                                            donutHoleColor: Colors.transparent,
+                                            sectionLabelType:
+                                                PieChartSectionLabelType.value,
+                                            sectionLabelStyle:
+                                                FlutterFlowTheme.of(context)
+                                                    .headlineSmall
+                                                    .override(
+                                                      fontFamily: 'Roboto',
+                                                      fontSize: 12.0,
+                                                      letterSpacing: 0.0,
+                                                      fontWeight:
+                                                          FontWeight.normal,
+                                                    ),
+                                            labelFormatter: LabelFormatter(
+                                              numberFormat: (val) =>
+                                                  formatNumber(
+                                                val,
+                                                formatType: FormatType.custom,
+                                                format: '0',
+                                                locale: 'pt-BR',
+                                              ),
+                                            ),
                                           ),
-                                        ),
+                                          Align(
+                                            alignment:
+                                                AlignmentDirectional(1.0, 1.0),
+                                            child: FlutterFlowChartLegendWidget(
+                                              entries: _model.descricao
+                                                  .asMap()
+                                                  .entries
+                                                  .map(
+                                                    (label) => LegendEntry(
+                                                      chartPieChartColorsList[label
+                                                              .key %
+                                                          chartPieChartColorsList
+                                                              .length],
+                                                      label.value,
+                                                    ),
+                                                  )
+                                                  .toList(),
+                                              width: 100.0,
+                                              height: 50.0,
+                                              textStyle:
+                                                  FlutterFlowTheme.of(context)
+                                                      .bodyMedium
+                                                      .override(
+                                                        fontFamily: 'Inter',
+                                                        letterSpacing: 0.0,
+                                                      ),
+                                              textPadding: EdgeInsetsDirectional
+                                                  .fromSTEB(5.0, 0.0, 0.0, 0.0),
+                                              padding: EdgeInsetsDirectional
+                                                  .fromSTEB(5.0, 0.0, 5.0, 0.0),
+                                              borderWidth: 1.0,
+                                              borderColor: Colors.black,
+                                              indicatorSize: 10.0,
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     ),
                                   ),
