62b89c0f351588d660d11c78fc3a313463812a49
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-26 18:19:05 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/viagens_recentes/viagens_recentes_widget.dart
+++ lib/pages/viagens_recentes/viagens_recentes_widget.dart
@@ -83,42 +83,47 @@ class _ViagensRecentesWidgetState extends State<ViagensRecentesWidget> {
         ),
         body: SafeArea(
           top: true,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(24.0, 10.0, 0.0, 10.0),
-                child: Text(
-                  'Esta é iuma lista das viagens recentes',
-                  textAlign: TextAlign.start,
-                  style: FlutterFlowTheme.of(context).labelMedium.override(
-                        fontFamily: 'Inter',
-                        letterSpacing: 0.0,
-                      ),
+          child: SingleChildScrollView(
+            primary: false,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                      EdgeInsetsDirectional.fromSTEB(24.0, 10.0, 0.0, 10.0),
+                  child: Text(
+                    'Esta é iuma lista das viagens recentes',
+                    textAlign: TextAlign.start,
+                    style: FlutterFlowTheme.of(context).labelMedium.override(
+                          fontFamily: 'Inter',
+                          letterSpacing: 0.0,
+                        ),
+                  ),
                 ),
-              ),
-              Builder(
-                builder: (context) {
-                  final viagens =
-                      FFAppState().ViagemResponseAppState.viagemList.toList();
+                Builder(
+                  builder: (context) {
+                    final viagens =
+                        FFAppState().ViagemResponseAppState.viagemList.toList();
 
-                  return ListView.builder(
-                    padding: EdgeInsets.zero,
-                    shrinkWrap: true,
-                    scrollDirection: Axis.vertical,
-                    itemCount: viagens.length,
-                    itemBuilder: (context, viagensIndex) {
-                      final viagensItem = viagens[viagensIndex];
-                      return ViagemDetalheSimplesWidget(
-                        key: Key('Key6py_${viagensIndex}_of_${viagens.length}'),
-                        viagem: viagensItem,
-                      );
-                    },
-                  );
-                },
-              ),
-            ],
+                    return ListView.builder(
+                      padding: EdgeInsets.zero,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      itemCount: viagens.length,
+                      itemBuilder: (context, viagensIndex) {
+                        final viagensItem = viagens[viagensIndex];
+                        return ViagemDetalheSimplesWidget(
+                          key: Key(
+                              'Key93z_${viagensIndex}_of_${viagens.length}'),
+                          viagem: viagensItem,
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
