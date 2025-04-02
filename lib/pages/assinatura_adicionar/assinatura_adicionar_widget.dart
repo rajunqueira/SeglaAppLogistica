69a4db1b90bae20005c134fadfd86bb85363cdb0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-04-02 22:16:23 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/assinatura_adicionar/assinatura_adicionar_widget.dart
+++ lib/pages/assinatura_adicionar/assinatura_adicionar_widget.dart
@@ -97,10 +97,10 @@ class _AssinaturaAdicionarWidgetState extends State<AssinaturaAdicionarWidget> {
                 padding: EdgeInsets.all(6.0),
                 child: Container(
                   width: MediaQuery.sizeOf(context).width * 1.0,
-                  height: 160.0,
+                  height: 240.0,
                   child: custom_widgets.SignatureWidget(
                     width: MediaQuery.sizeOf(context).width * 1.0,
-                    height: 160.0,
+                    height: 240.0,
                   ),
                 ),
               ),
