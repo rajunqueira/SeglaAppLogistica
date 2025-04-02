8a4434ba1413432aecce5aeec201946d34ad4cf1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-04-02 15:05:54 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/coleta_concluir/coleta_concluir_widget.dart
+++ lib/pages/coleta_concluir/coleta_concluir_widget.dart
@@ -410,6 +410,7 @@ class _ColetaConcluirWidgetState extends State<ColetaConcluirWidget> {
                             int.tryParse(_model.odometroTextController.text),
                         dataHoraFim:
                             functions.todayToDateTime(getCurrentTimestamp),
+                        assinaturaCliente: _model.ass1,
                       );
 
                       if ((_model.apiResultatf?.succeeded ?? true)) {
