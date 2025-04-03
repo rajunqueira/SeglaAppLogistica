1baae5c12eccdfc2e8a9e58368d59ec1cfd209bc
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-04-03 02:41:51 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/assinatura_adicionar/assinatura_adicionar_widget.dart
+++ lib/pages/assinatura_adicionar/assinatura_adicionar_widget.dart
@@ -6,6 +6,7 @@ import 'dart:ui';
 import '/custom_code/actions/index.dart' as actions;
 import '/custom_code/widgets/index.dart' as custom_widgets;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'assinatura_adicionar_model.dart';
@@ -31,6 +32,11 @@ class _AssinaturaAdicionarWidgetState extends State<AssinaturaAdicionarWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => AssinaturaAdicionarModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      await actions.clearSignature();
+    });
   }
 
   @override
