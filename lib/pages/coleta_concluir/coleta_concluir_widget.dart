d2bf7f69fccb08af047c81075555dcf601e6f886
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-04-03 12:56:02 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/coleta_concluir/coleta_concluir_widget.dart
+++ lib/pages/coleta_concluir/coleta_concluir_widget.dart
@@ -302,6 +302,19 @@ class _ColetaConcluirWidgetState extends State<ColetaConcluirWidget> {
                                     ],
                                   ),
                                 ),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  child: Image.memory(
+                                    functions
+                                            .base64DecodeToImage(
+                                                FFAppState().tempImage)
+                                            .bytes ??
+                                        Uint8List.fromList([]),
+                                    width: 200.0,
+                                    height: 200.0,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
                                 Row(
                                   mainAxisSize: MainAxisSize.max,
                                   mainAxisAlignment:
